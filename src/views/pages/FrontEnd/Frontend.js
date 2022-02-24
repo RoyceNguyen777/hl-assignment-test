872a63c2917ce571b7d01ca83191f928a7ff3fbf
DESKTOP-CRSKLT8\Thuy-Nguyen <nguyenthahthu2014@gmail.com>
2022-02-24 15:56:30 +0700

    footer fix

--- src/views/pages/FrontEnd/Frontend.js
+++ src/views/pages/FrontEnd/Frontend.js
@@ -137,7 +137,7 @@ export default function Frontend() {
           <StyleParaph>
             This website is created as part of Hlsolutions program. The
             materials  contained on this website are provided for general
-            information only and <br /> do not constitute  any form of advice. HLS
+            information only and  do not constitute  any form of advice. HLS
             assumes no responsibility for the accuracy of any particular
             statement and accepts no liability for any loss or damage which may
             arise from reliance on the information contained on this site.
